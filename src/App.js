3cdd20fbef87600e8e5f4488175f61c157906420
karpovvicz <karpovvicz@gmail.com>
2025-07-25 16:20:35 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
+
 import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
+import Header from "./components/Header";
 
 const concepts = [
   {
@@ -27,17 +28,24 @@ const concepts = [
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header />
       <ul id="concepts">
         <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
+          <img src={concepts[0].image} alt={concepts[0].title}/>
+          <h2>{concepts[0].title}</h2>
+          <p>{concepts[0].description}</p>
         </li>
+        <li className="concept">
+          <img src={concepts[1].image} alt={concepts[0].title}/>
+          <h2>{concepts[1].title}</h2>
+          <p>{concepts[1].description}</p>
+        </li>
+        <li className="concept">
+          <img src={concepts[2].image} alt={concepts[0].title}/>
+          <h2>{concepts[2].title}</h2>
+          <p>{concepts[2].description}</p>
+        </li>
+
       </ul>
     </div>
   );
